6eb29290eed5f18af32efde4fc6cc25856c9c64c
Kala <kalashnikxvxiii@gmail.com>
2025-11-13 19:28:58 +0100

    style: format test_cli.py with Black

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -440,6 +440,7 @@ class TestMainCommand:
     def test_main_version(self, runner):
         """Should display version."""
         from stegvault import __version__
+
         result = runner.invoke(main, ["--version"])
 
         assert result.exit_code == 0
